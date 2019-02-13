e879e1d822f964b57be5c2f460059a5cfed7858f
VincentKempers <me@vncn.tk>
2019-02-13 16:41:06 +0100

    add icon/ add animation on the grableave and enter/ WIP animation on dropping

--- opdracht1/v1/script.js
+++ opdracht1/v1/script.js
@@ -47,6 +47,7 @@ var songArray = [
 var playlist = document.querySelector("ol");
 var addSong = document.querySelector("button");
 var counter = document.querySelector("span");
+
 counter.innerHTML = "Songs: " + songArray.length;
 
 for (let i = 0; i < songArray.length; i++) {
@@ -77,6 +78,7 @@ for (let i = 0; i < songArray.length; i++) {
             </g>
           </svg>
         </p>
+        <div id="borderSet"></div>
     </li>`
   );
 }
@@ -103,25 +105,9 @@ addSong.addEventListener(
         <p class="duration">${addSongInfo.duration}</p>
         <p>${addSongInfo.date}</p>
         <p id="dragOption">
-          <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 438.533 438.533">
-            <g>
-              <g>
-              <path d="M420.265,328.897H18.274c-4.952,0-9.235,1.813-12.851,5.428C1.807,337.938,0,342.224,0,347.172v36.548
-              c0,4.949,1.807,9.23,5.424,12.848c3.619,3.613,7.902,5.424,12.851,5.424h401.991c4.948,0,9.229-1.811,12.847-5.424
-              c3.614-3.617,5.421-7.898,5.421-12.848v-36.548c0-4.948-1.8-9.233-5.421-12.847C429.495,330.711,425.217,328.897,420.265,328.897z
-              "/>
-              <path d="M433.112,41.968c-3.617-3.617-7.898-5.426-12.847-5.426H18.274c-4.952,0-9.235,1.809-12.851,5.426
-              C1.807,45.583,0,49.866,0,54.813V91.36c0,4.949,1.807,9.229,5.424,12.847c3.619,3.618,7.902,5.424,12.851,5.424h401.991
-              c4.948,0,9.229-1.807,12.847-5.424c3.614-3.617,5.421-7.898,5.421-12.847V54.813C438.533,49.866,436.729,45.583,433.112,41.968z"
-              />
-              <path d="M420.265,182.72H18.274c-4.952,0-9.235,1.809-12.851,5.426C1.807,191.761,0,196.044,0,200.992v36.547
-              c0,4.948,1.807,9.236,5.424,12.847c3.619,3.614,7.902,5.428,12.851,5.428h401.991c4.948,0,9.229-1.813,12.847-5.428
-              c3.614-3.61,5.421-7.898,5.421-12.847v-36.547c0-4.948-1.807-9.231-5.421-12.847C429.495,184.528,425.217,182.72,420.265,182.72z"
-              />
-              </g>
-            </g>
-          </svg>
+          <img src="rassets/icon.svg" alt="icon">
         </p>
+        <div id="borderSet"></div>
     </li>`
     );
 
@@ -180,7 +166,6 @@ document.addEventListener(
     // highlight potential drop target when the draggable element enters it
     if (event.target.className == "dropzone") {
       event.target.setAttribute("id", "over");
-      console.log(event.target, "enter");
     }
   },
   false
@@ -200,19 +185,24 @@ document.addEventListener(
   false
 );
 
+var borderAnim = document.querySelector("#borderSet");
+
 document.addEventListener(
   "drop",
   function(event) {
     // prevent default action (open as link for some elements)
+
     event.preventDefault();
     // move dragged elem to the selected drop target
     if (event.target.className == "dropzone") {
       // don't show the border anymore
       event.target.style.borderBottom = "";
+
       // remove the id "over"
       event.target.removeAttribute("id", "over");
       // Remove the child beneath
       dragged.parentNode.removeChild(dragged);
+      console.log(dragged);
       // add html after the end of the selected target.
       event.target.insertAdjacentHTML(
         "afterend",
@@ -222,25 +212,9 @@ document.addEventListener(
         <p class="duration">${dragged.children[2].textContent}</p>
         <p>${dragged.children[3].textContent}</p>
         <p id="dragOption">
-          <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 438.533 438.533">
-            <g>
-              <g>
-              <path d="M420.265,328.897H18.274c-4.952,0-9.235,1.813-12.851,5.428C1.807,337.938,0,342.224,0,347.172v36.548
-              c0,4.949,1.807,9.23,5.424,12.848c3.619,3.613,7.902,5.424,12.851,5.424h401.991c4.948,0,9.229-1.811,12.847-5.424
-              c3.614-3.617,5.421-7.898,5.421-12.848v-36.548c0-4.948-1.8-9.233-5.421-12.847C429.495,330.711,425.217,328.897,420.265,328.897z
-              "/>
-              <path d="M433.112,41.968c-3.617-3.617-7.898-5.426-12.847-5.426H18.274c-4.952,0-9.235,1.809-12.851,5.426
-              C1.807,45.583,0,49.866,0,54.813V91.36c0,4.949,1.807,9.229,5.424,12.847c3.619,3.618,7.902,5.424,12.851,5.424h401.991
-              c4.948,0,9.229-1.807,12.847-5.424c3.614-3.617,5.421-7.898,5.421-12.847V54.813C438.533,49.866,436.729,45.583,433.112,41.968z"
-              />
-              <path d="M420.265,182.72H18.274c-4.952,0-9.235,1.809-12.851,5.426C1.807,191.761,0,196.044,0,200.992v36.547
-              c0,4.948,1.807,9.236,5.424,12.847c3.619,3.614,7.902,5.428,12.851,5.428h401.991c4.948,0,9.229-1.813,12.847-5.428
-              c3.614-3.61,5.421-7.898,5.421-12.847v-36.547c0-4.948-1.807-9.231-5.421-12.847C429.495,184.528,425.217,182.72,420.265,182.72z"
-              />
-              </g>
-            </g>
-          </svg>
+        ${dragged.children[4].textContent}
         </p>
+        <div id="borderSet" class="borderAnim"></div>
         </li>`
       );
     }
