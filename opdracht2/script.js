c34a18b4bf63d4627cbd510043e8df667645ca5b
Vincent Kempers <me@vncn.tk>
2019-02-27 18:45:30 +0100

    add interactivity when pressing e

---
+++ opdracht2/script.js
@@ -0,0 +1,14 @@
+var allSections = $("section");
+console.log(allSections);
+
+$("html, body").keyup("e", function() {
+  window.location.hash = "#work";
+});
+
+function nextSection() {
+  // go to the nest section
+}
+
+function prevSection() {
+  // go to the previous section
+}
